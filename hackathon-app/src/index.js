ce5737d07a64bc907e3c013ba10e76682c0e50cd
YuPei <sunyupei1999@gmail.com>
2021-12-13 11:54:48 +0800

    add a shitty calendar

--- hackathon-app/src/index.js
+++ hackathon-app/src/index.js
@@ -1,36 +1,40 @@
 import React from 'react';
 import { useState } from "react";
 import ReactDOM from "react-dom";
+import Calendar from "react-calendar"
+import 'react-calendar/dist/Calendar.css';
 
 function Test() {
-  const [car, setCar] = useState({
-    brand: "Ford",
-    model: "Mustang",
-    year: "1964",
-    color: "red"
-  });
 
-  const updateColor = () => {
-    setCar(previousState => {
-      if (previousState["color"] === "blue") {
-        return { ...previousState, color: "red" }
-      } else {
-        return { ...previousState, color: "blue" }
-      }
+    const [car, setCar] = useState({
+        brand: "Ford",
+        model: "Mustang",
+        year: "1964",
+        color: "red"
     });
-  }
 
-  return (
-    <>
-      <h1>My {car.brand}</h1>
-      <p>
-        It is a {car.color} {car.model} from {car.year}.
-      </p>
-      <button
-        type="button"
-        onClick={updateColor}
-      >Change Color</button>
-    </>
+    const updateColor = () => {
+        setCar(previousState => {
+            if (previousState["color"] === "blue") {
+                return { ...previousState, color: "red" }
+            } else {
+                return { ...previousState, color: "blue" }
+            }
+        });
+    }
+
+    return (
+        <>
+            <Calendar/>
+            <h1>My {car.brand}</h1>
+            <p>
+                It is a {car.color} {car.model} from {car.year}.
+            </p>
+            <button
+                type="button"
+                onClick={updateColor}
+            >Change Color</button>
+        </>
   )
 }
 
